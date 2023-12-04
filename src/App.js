aadf20817702dbe56498cd652a9eb0c83f416f6e
rchellee <ruerasritchelle07@gmail.com>
2023-12-05 01:55:28 +0800

    updated app js and css
    created exercise, jsx, props, css and components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-
-const name = <h1> Ritchelle </h1>
-const age = <h1> 21 </h1>
-const email = <h1> ritchellerueras@gmail.com </h1>
-const user = (<div>
-      {name}
-      {age}
-      {email}
-  </div>)
-
   return (
     <div className="App">
-      {user}
-      {user}
-      {user}
-
-
+      <h1 className="name"> Ritchelle </h1>
+      <h1 className="title-1"> Bachelor of Science in Information System </h1>
     </div>
   );
 }
