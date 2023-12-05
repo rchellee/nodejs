c527e6702a2627621d171c0b40d2926fcb1d4424
rchellee <ruerasritchelle07@gmail.com>
2023-12-05 12:32:43 +0800

    created user.js
    created exercises

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import "./App.css";
+import { User } from'./User';
 
 function App() {
+
+  const planets = [
+    { planetName: "Earth", isRockPlanet: true },
+    { planetName: "Mars", isRockPlanet: true },
+    { planetName: "Venus", isRockPlanet: true },
+    { planetName: "Jupiter", isRockPlanet: false },
+    { planetName: "Neptune", isRockPlanet: false },
+    { planetName: "Uranus", isRockPlanet: false },
+    { planetName: "Mercury", isRockPlanet: true },
+    ];
+
+  // return <div className='App'>
+  //   <h2>Planets</h2>
+  //   {planets.map((user, key) => {
+  //     return <User planetName={user.planetName} isRockPlanet={user.isRockPlanet=false} />
+  //   })}
+  //   <h2>Gas Planets</h2>
+  // </div>
+  const allPlanets = planets.map((planet, key) => <User key={key} planetName={planet.planetName} />);
+  const gasPlanets = planets.filter(planet => !planet.isRockPlanet).map((planet, key) => <User key={key} planetName={planet.planetName} />);
+
   return (
-    <div className="App">
-      <h1 className="name"> Ritchelle </h1>
-      <h1 className="title-1"> Bachelor of Science in Information System </h1>
+    <div className='App'>
+      <h1 className='name'>Planets</h1>
+      <h2 className='title-1'>{allPlanets} </h2>
+
+      <h1 className='name'>Gas Planets</h1>
+      <h2 className='title-1'>{gasPlanets} </h2>
     </div>
   );
+
 }
+
 
 export default App;
