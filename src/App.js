fc32d822122676fa8f2fdb545f2653e49f7e7851
rchellee <ruerasritchelle07@gmail.com>
2023-12-16 20:17:11 +0800

    created ToDOList
    Created a React Application that will Create, Read, Update, Delete information

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import "./App.css";
-import { useState } from "react";
+import { TodoWrapper } from "./components/TodoWrapper";
 
 function App() {
-  const [numbr, setNumbr] = useState(1);
-  const increaseNumbr = () => {
-    setNumbr(numbr + 1);
-  };
-
-  const decreaseNumbr = () => {
-    setNumbr(numbr - 1);
-  };
-
-  const setToZero = () => {
-    setNumbr(0);
-  };
   return (
     <div className="App">
-      <button onClick={increaseNumbr}> Increment </button>
-      <button onClick={decreaseNumbr}> Decrement </button>
-      <button onClick={setToZero}> Zero </button>
-      <h1>{numbr}</h1>
+      <TodoWrapper />
     </div>
   );
 }
